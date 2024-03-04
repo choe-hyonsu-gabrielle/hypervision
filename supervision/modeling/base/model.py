76525d11dc61cd605f33fa511a3492ce9fd74b59
Choe Hyonsu Gabrielle <choe.hyonsu.gabrielle@gmail.com>
2024-03-04 14:35:34 +0900

    Create model.py

---
+++ supervision/modeling/base/model.py
@@ -0,0 +1,53 @@
+import abc
+from typing import Any
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.types import OptimizerLRScheduler, STEP_OUTPUT
+from transformers import PreTrainedModel, PreTrainedTokenizer
+from supervision.modeling.base.config import ModelConfigBase
+
+
+class LightningModuleBase(pl.LightningModule):
+    # This is a base class of customized LightningModule variants with commonly features are pre-implemented.
+    def __init__(self, config: ModelConfigBase, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # model configs & peripherals
+        self.config = config
+
+        # core components
+        self.pretrained_tokenizer: PreTrainedTokenizer = self.config.pretrained_tokenizer
+        self.pretrained_model: PreTrainedModel = self.config.pretrained_model.to(self.device)
+        self.downstream_task_head = None
+
+    def configure_optimizers(self) -> OptimizerLRScheduler:
+        optimizer = self.config.optimizer(self.parameters(), lr=self.config.learning_rate)
+        if self.config.lr_scheduler:
+            lr_scheduler_config = dict(
+                scheduler=self.config.lr_scheduler(optimizer, **self.config.lr_scheduler_params),
+                interval='step',
+                name=self.config.lr_scheduler.__class__.__name__
+            )
+            return [optimizer], [lr_scheduler_config]
+        return optimizer
+
+    @abc.abstractmethod
+    def batch_forward_and_loss(self, samples: Any) -> torch.Tensor:
+        pass
+
+    def training_step(self, samples, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        loss = self.batch_forward_and_loss(samples)
+        self.log(name='train_loss', value=loss, prog_bar=True, sync_dist=True)
+        if self.config.lr_scheduler:
+            self.log('learning_rate', self.lr_schedulers().get_last_lr()[0], prog_bar=True, sync_dist=True)
+        return dict(loss=loss)
+
+    def validation_step(self, samples, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        loss = self.batch_forward_and_loss(samples)
+        self.log(name='valid_loss', value=loss, batch_size=self.config.batch_size, prog_bar=True, sync_dist=True)
+        return dict(loss=loss)
+
+    def test_step(self, samples, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
+        loss = self.batch_forward_and_loss(samples)
+        self.log(name='test_loss', value=loss, batch_size=self.config.batch_size, prog_bar=True, sync_dist=True)
+        return dict(loss=loss)
