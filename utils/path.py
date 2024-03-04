a761031fc8ece121149ee9176d71c258fb08b0bf
Choe Hyonsu Gabrielle <choe.hyonsu.gabrielle@gmail.com>
2024-03-04 14:53:41 +0900

    Create path.py

---
+++ utils/path.py
@@ -0,0 +1,16 @@
+import glob
+from os.path import abspath
+
+
+def filepath_resolution(filepath_or_pathlike: (str, list), absolute: bool = None):
+    filepaths = []
+    if filepath_or_pathlike is None:
+        return None
+    elif isinstance(filepath_or_pathlike, str):
+        filepaths.extend(glob.glob(filepath_or_pathlike))
+    elif isinstance(filepath_or_pathlike, list):
+        for f in filepath_or_pathlike:
+            filepaths.extend(glob.glob(f))
+    if absolute:
+        return [abspath(p) for p in filepaths]
+    return filepaths
