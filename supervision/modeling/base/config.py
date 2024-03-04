0ea55b0ae7d2c193aff6c61754f6d17eefc1b726
Choe Hyonsu Gabrielle <choe.hyonsu.gabrielle@gmail.com>
2024-03-04 14:34:28 +0900

    Update config.py

--- supervision/modeling/base/config.py
+++ supervision/modeling/base/config.py
@@ -1,1 +1,50 @@
+from transformers import AutoConfig, AutoTokenizer, AutoModel
 
+
+class ModelConfigBase:
+    def __init__(self, pretrained_model_name_or_path: str):
+        self.pretrained_model_name_or_path = pretrained_model_name_or_path
+        self._pretrained_config = None
+        self._pretrained_tokenizer = None
+        self._pretrained_model = None
+
+        # pre-determined hyper-params of pretrained model
+        self.pretrained_model_hidden_size = None
+        self.pretrained_model_max_token_length = None
+        self.num_classes = None
+
+        # Notes: these values are will be determined right before training
+        self.batch_size = None
+        self.learning_rate = None
+        self.objective = None
+        self.activation = None
+
+        # optimizer and scheduler
+        self.optimizer = None
+        self.lr_scheduler = None
+        self.lr_scheduler_params = None
+
+        # if you want to save pretrained things:
+        # self.pretrained_tokenizer.save_pretrained('./pretrained_tokenizer/')
+        # self.pretrained_model.save_pretrained('./pretrained_encoder/')
+
+    @property
+    def pretrained_config(self):
+        if self._pretrained_config is None:
+            print(f'[{self.__class__.__name__}] loading pretrained config of `{self.pretrained_model_name_or_path}`')
+            self._pretrained_config = AutoConfig.from_pretrained(self.pretrained_model_name_or_path)
+        return self._pretrained_config
+
+    @property
+    def pretrained_tokenizer(self):
+        if self._pretrained_tokenizer is None:
+            print(f'[{self.__class__.__name__}] loading pretrained tokenizer of `{self.pretrained_model_name_or_path}`')
+            self._pretrained_tokenizer = AutoTokenizer.from_pretrained(self.pretrained_model_name_or_path)
+        return self._pretrained_tokenizer
+
+    @property
+    def pretrained_model(self):
+        if self._pretrained_model is None:
+            print(f'[{self.__class__.__name__}] loading pretrained model of `{self.pretrained_model_name_or_path}`')
+            self._pretrained_model = AutoModel.from_pretrained(self.pretrained_model_name_or_path)
+        return self._pretrained_model
