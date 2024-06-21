6060e66d5c6d5a21550bb4099416d7dc60e748f1
Choe Hyonsu Gabrielle <choe.hyonsu.gabrielle@gmail.com>
2024-06-21 20:48:28 +0900

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,14 +1,16 @@
+from typing import Any
 from torch.utils.data import Dataset
-from utils.path import filepath_resolution
-from utils.json import load_jsons
-from utils.csv import load_csvs
+from utility.path import filepath_resolution
+from utility.json import load_jsons
+from utility.csv import load_csvs
+from utility.tsv import load_tsvs
 
 
 class DatasetBase(Dataset):
     def __init__(self, filenames: list[str]):
         self.filenames = filepath_resolution(filenames)
         print(f'[{self.__class__.__name__}] initializing Dataset from: {self.filenames}')
-        self.data: list[dict] = None
+        self.data: Any = None
 
     def __len__(self):
         return len(self.data)
@@ -28,6 +30,12 @@ class MultipleCSVsDataset(DatasetBase):
     def __init__(self, filenames: list[str]):
         super().__init__(filenames)
         self.data = load_csvs(self.filenames, encoding='utf-8', flatten=True)
+
+class MultipleTSVsDataset(DatasetBase):
+    def __init__(self, filenames: List[str], first_as_column_names: bool = True, map_columns: list = None):
+        super().__init__(filenames)
+        self.data = load_tsvs(self.filenames, encoding='utf-8', flatten=True,
+                              first_as_column_names=first_as_column_names, map_columns=map_columns)
 
 
 if __name__ == '__main__':
